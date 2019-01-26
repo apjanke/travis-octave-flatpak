4f64b01707693273758fa38bff5aaeefbcff4d0b
Andrew Janke <floss@apjanke.net>
2019-01-26 01:15:52 -0500

    git in there!

---
+++ dev-tools/install-octave-flatpak.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This installs Octave 4.4 on Ubuntu using Flatpak.
+#
+# This should work on Trusty, Xenial, and Bionic.
+
+echo $0: installing flatpak
+if grep -i 'xenial\|trusty' /etc/lsb-release &>/dev/null; then
+	echo $0: Adding apt repository ppa:alexlarsson/flatpak to get flatpak Octave 4.4.
+	sudo add-apt-repository ppa:alexlarsson/flatpak --yes
+	sudo apt-get update
+fi
+sudo apt-get install --yes flatpak
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install -y flathub org.octave.Octave
+# Hack to get this flatpak octave and mkoctfile on the path
+octave_dir=$(flatpak info org.octave.Octave | grep Location | cut -d ":" -f 2 | cut -d " " -f 2)
+ln -s "$octave_dir/files/bin/octave" ~/bin
+ln -s "$octave_dir/files/bin/mkoctfile" ~/bin
